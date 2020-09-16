68e99ddb07e9df8fed6f347292c9c1b3bd54df34
Sheng <28744573+s224j31s@users.noreply.github.com>
2020-09-16 21:01:19 +1000

    Add files via upload

--- BankConsoleApp/Accounts.cs
+++ BankConsoleApp/Accounts.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BankConsoleApp
 {
-    class Accounts
+    public class Accounts
     {
-        private readonly int accNumber;
+        private int accNumber;
         private string firstName;
         private string lastName;
         private string address;
@@ -16,9 +17,9 @@ namespace BankConsoleApp
         private double balance;
         private string email;
 
-        public Accounts(int accNumber, string firstName, string lastName, string address, string phoneNo, double balance, string email)
+        public Accounts(string firstName, string lastName, string address, string phoneNo, string email, int accNumber, double balance)
         {
-            this.accNumber = accNumber;
+            this.AccNumber = accNumber;
             this.firstName = firstName;
             this.lastName = lastName;
             this.address = address;
@@ -27,22 +28,83 @@ namespace BankConsoleApp
             this.email = email;
         }
 
-        public void deposit(double amount)
+        public void Deposit(double amount)
         {
-            balance += amount; 
+            balance += amount;
+            string FileName = Path.Combine(accNumber.ToString()) + ".txt";//text file joining with account number
+
+            //File.WriteAllText(string.Format($"{accNumber}.txt"),
+            //string.Format($"{accountNumber}\n{firstName}\n{lastName}\n" +
+            //$"{address}\n{phoNumber}\n{balance:0.00}\n{email}\n{statementStringBlock}"));
+            using (StreamWriter accDetails = new StreamWriter(FileName))//update account balance
+            {
+                accDetails.WriteLine("First Name|" + firstName);
+                accDetails.WriteLine("Last Name|" + lastName);
+                accDetails.WriteLine("address|" + address);
+                accDetails.WriteLine("Phone Number|" + phoneNo);
+                accDetails.WriteLine("email|" + email);
+                accDetails.WriteLine("Account Number|" + accNumber);
+                accDetails.WriteLine("Balance|" + balance);
+            }
+
         }
 
-        public void withdraw(double amount) {
+        public void Withdraw(double amount) {
+            string FileName = Path.Combine(accNumber.ToString()) + ".txt";//text file joining with account number
+
             if (balance >= amount) {
                 balance -= amount;
+                using (StreamWriter accDetails = new StreamWriter(FileName))//update account balance
+                {
+                    accDetails.WriteLine("First Name|" + firstName);
+                    accDetails.WriteLine("Last Name|" + lastName);
+                    accDetails.WriteLine("address|" + address);
+                    accDetails.WriteLine("Phone Number|" + phoneNo);
+                    accDetails.WriteLine("email|" + email);
+                    accDetails.WriteLine("Account Number|" + accNumber);
+                    accDetails.WriteLine("Balance|" + balance);
+
+                }
             }
+
+        }
+        public bool matchNumbers(int accountNumber)
+        {
+            return this.AccNumber == accountNumber;
         }
 
+        public bool checkBalance(double enoughBalance)
+        {
+            return balance >= enoughBalance;
+        }
+
+        public void viewAccDetails()
+        {
+
+            Console.WriteLine();
+            Console.WriteLine(" ╔════════════════════════════════════════════════╗");
+            Console.WriteLine(" ║                                                ║");
+            Console.WriteLine(" ║                 ACCOUNT DETAILS                ║");
+            Console.WriteLine(" ║════════════════════════════════════════════════║");
+            Console.WriteLine(" ║                                                ║");
+            Console.WriteLine($" ║    Account Number: {accNumber}".PadRight(50, ' ') + "║");
+            Console.WriteLine($" ║    Account Balance: ${balance:0.00}".PadRight(50, ' ') + "║");
+            Console.WriteLine($" ║    First Name: {firstName}".PadRight(50, ' ') + "║");
+            Console.WriteLine($" ║    Last Name: {lastName}".PadRight(50, ' ') + "║");
+            Console.WriteLine($" ║    Address: {address}".PadRight(50, ' ') + "║");
+            Console.WriteLine($" ║    Phone: {phoneNo}".PadRight(50, ' ') + "║");
+            Console.WriteLine($" ║    Email: {email}".PadRight(50, ' ') + "║");
+            Console.WriteLine(" ╚════════════════════════════════════════════════╝");
+            Console.WriteLine();
+        }
         public string FirstName { get => firstName; set => firstName = value; }
         public string LastName { get => lastName; set => lastName = value; }
         public string Address { get => address; set => address = value; }
         public string PhoneNo { get => phoneNo; set => phoneNo = value; }
         public string Email { get => email; set => email = value; }
         public double Balance { get => balance; set => balance = value; }
+        public int AccNumber { get => accNumber; set => accNumber = value; }
+
+
     }
 }
