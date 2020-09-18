15d62f7fc6f3898676376ad688e4f3210edb6271
Sheng <28744573+s224j31s@users.noreply.github.com>
2020-09-18 21:37:42 +1000

    Add files via upload
    email functionality for create account

--- BankConsoleApp/Accounts.cs
+++ BankConsoleApp/Accounts.cs
@@ -16,6 +16,7 @@ namespace BankConsoleApp
         private string phoneNo;
         private double balance;
         private string email;
+        private List<Transactions> bankStatement;
 
         public Accounts(string firstName, string lastName, string address, string phoneNo, string email, int accNumber, double balance)
         {
@@ -26,16 +27,21 @@ namespace BankConsoleApp
             this.phoneNo = phoneNo;
             this.balance = balance;
             this.email = email;
+            bankStatement = new List<Transactions>();
         }
-
+        //method for depositing money into an account
         public void Deposit(double amount)
         {
             balance += amount;
+            depositStatement(amount);
             string FileName = Path.Combine(accNumber.ToString()) + ".txt";//text file joining with account number
 
             //File.WriteAllText(string.Format($"{accNumber}.txt"),
             //string.Format($"{accountNumber}\n{firstName}\n{lastName}\n" +
             //$"{address}\n{phoNumber}\n{balance:0.00}\n{email}\n{statementStringBlock}"));
+            string statementLists = "";
+            foreach (Transactions transaction in bankStatement)
+                statementLists += transaction.fileString();
             using (StreamWriter accDetails = new StreamWriter(FileName))//update account balance
             {
                 accDetails.WriteLine("First Name|" + firstName);
@@ -45,15 +51,26 @@ namespace BankConsoleApp
                 accDetails.WriteLine("email|" + email);
                 accDetails.WriteLine("Account Number|" + accNumber);
                 accDetails.WriteLine("Balance|" + balance);
+                accDetails.WriteLine(statementLists);
             }
 
         }
+
+        private void depositStatement(double amount) {
+            bankStatement.Add(new Transactions(DateTime.Now, "Deposit", amount, balance));
+        }
+
+
 
         public void Withdraw(double amount) {
             string FileName = Path.Combine(accNumber.ToString()) + ".txt";//text file joining with account number
 
             if (balance >= amount) {
                 balance -= amount;
+                withdrawStatement(amount);
+                string statementLists = "";
+                foreach (Transactions transaction in bankStatement)
+                    statementLists += transaction.fileString();
                 using (StreamWriter accDetails = new StreamWriter(FileName))//update account balance
                 {
                     accDetails.WriteLine("First Name|" + firstName);
@@ -63,11 +80,32 @@ namespace BankConsoleApp
                     accDetails.WriteLine("email|" + email);
                     accDetails.WriteLine("Account Number|" + accNumber);
                     accDetails.WriteLine("Balance|" + balance);
+                    accDetails.WriteLine(statementLists);
 
                 }
             }
 
         }
+
+        private void withdrawStatement(double amount)
+        {
+            bankStatement.Add(new Transactions(DateTime.Now, "Withdraw", amount, balance));
+        }
+        public void AddOldStatements()
+        {
+        
+            string[] accountFile = File.ReadAllLines($"{accNumber}.txt");
+            string[] oldStatements = accountFile.Skip(7).ToArray();
+            foreach (string transaction in oldStatements)
+            {
+                char[] delimiter = { '|' };
+                string[] txnInfo = transaction.Split(delimiter);
+                bankStatement.Add(new Transactions(Convert.ToDateTime(txnInfo[0]), txnInfo[1], Convert.ToDouble(txnInfo[2]),
+                        Convert.ToDouble(txnInfo[3])));
+            }
+        }
+
+
         public bool matchNumbers(int accountNumber)
         {
             return this.AccNumber == accountNumber;
@@ -80,7 +118,6 @@ namespace BankConsoleApp
 
         public void viewAccDetails()
         {
-
             Console.WriteLine();
             Console.WriteLine(" ╔════════════════════════════════════════════════╗");
             Console.WriteLine(" ║                                                ║");
---
+++ BankConsoleApp/Transactions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BankConsoleApp
+{
+    public class Transactions
+    {
+        private DateTime date;
+        private string type;
+        private double debitCredit;
+        private double balance;
+
+        public Transactions(DateTime date, string type, double debitCredit, double balance)
+        {
+            this.date = date;
+            this.type = type;
+            this.debitCredit = debitCredit;
+            this.balance = balance;
+        }
+
+        public string fileString()
+        {
+            return string.Format($"{date.ToString("dd/MM/yyyy")}|" +
+                                 $"{type}|{debitCredit}|{balance}\n");
+        }
+    }
+}
